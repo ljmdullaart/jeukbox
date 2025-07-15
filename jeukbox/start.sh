f8ba78840e006cea9aa97473cc0955c756e2c8fb
ljmdullaart <ljm@acm.org>
2025-07-15 16:45:40 +0200

    aded another possible start path

--- jeukbox/start.sh
+++ jeukbox/start.sh
@@ -3,5 +3,7 @@ if [ -f bin/app.psgi ] ; then
 	plackup -p 5101 -r bin/app.psgi
 elif [ -f /opt/jeukbox/bin/app.psgi ] ; then
 	plackup -p 5101 -r /opt/jeukbox/bin/app.psgi
+elif [ -f /opt/jeukbox/jeukbox/bin/app.psgi ] ; then
+	plackup -p 5101 -r /opt/jeukbox/jeukbox/bin/app.psgi
 fi
 
